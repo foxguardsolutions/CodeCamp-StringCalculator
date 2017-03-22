18ac0ace0b22e11f474ffccf0c092a22babbc5af
Taylor Bittner <tbittner@foxguardsolutions.com>
2017-03-22 15:54:07 -0400

    Calculate the sum of 0, 1, or 2 numbers in a string
    The first step in the String Calculator Kata, calculate the sum of up to two numbers in a string separated by a `,`. Empty strings `""` will return a value of 0.

---
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        private Fixture _fixture;
+        private StringCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+            _calculator = new StringCalculator();
+        }
+        
+        [Test]
+        public void Add_GivenEmptyString_Returns0()
+        {
+            var emptyString = "";
+            var expected = 0;
+
+            var actual = _calculator.Add(emptyString);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Add_GivenSingleNumberString_ReturnsIntegerValue()
+        {
+            var expected = _fixture.Create<int>();
+
+            var actual = _calculator.Add(expected.ToString());
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Add_GivenTwoNumberString_ReturnsSumOfNumbers()
+        {
+            var number1 = _fixture.Create<int>();
+            var number2 = _fixture.Create<int>();
+            var numbers = number1 + "," + number2;
+            var expected = number1 + number2;
+
+            var actual = _calculator.Add(numbers);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }       
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        private const char COMMA = ',';
+
+        public int Add(string numbers)
+        {
+            var splitNumbers = numbers.Split(COMMA);
+            var sum = GetNumericValueFromString(splitNumbers[0]);
+
+            if (splitNumbers.Length > 1)
+                sum += GetNumericValueFromString(splitNumbers[1]);
+
+            return sum;
+        }
+
+        private int GetNumericValueFromString(string number)
+        {
+            return string.IsNullOrWhiteSpace(number) ? 0 : int.Parse(number);
+        }
+    }
+}
